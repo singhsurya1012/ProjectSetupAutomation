baa052339c86b0a80f223f15791372567d599cb4
Surya <singhsurya1012@gmail.com>
2020-08-28 22:38:05 +0530

    updating python script

--- Windows/createRepo.py
+++ Windows/createRepo.py
@@ -1,6 +1,6 @@
 import sys
 
 #Getting projectName from commandLine Argument passed from script
-projectName = str(sys.argv[1]);
+projectName = str(sys.argv[1])
 
-print("Creating repo")
+print("Creating repo: " + projectName)
