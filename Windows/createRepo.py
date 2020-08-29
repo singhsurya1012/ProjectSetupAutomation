f0d52525f7083abdef686dcb991ced4c34abe3c8
Surya <singhsurya1012@gmail.com>
2020-08-29 21:54:13 +0530

    Adding default gitignore file which will be added in every new repo

--- Windows/createRepo.py
+++ Windows/createRepo.py
@@ -1,30 +1,52 @@
-import sys
 import os
+import sys
 
-#Getting projectName from commandLine Argument passed from script
+from github import Github
+
+# Getting projectName from commandLine Argument passed from script
 projectName = str(sys.argv[1])
 
-#Geting project dir and github token from env variables\
+# Geting project dir and github token from env variables\
 projectDir = os.environ['proj_dir']
 gitToken = os.environ['git_token']
 
-projectDir = projectDir +'/' + projectName
+projectDir = projectDir + '/' + projectName
 
 if not os.path.exists(projectDir):
     os.mkdir(projectDir)
     print("Directory " , projectDir ,  " Created ")
-else:    
-    print("Directory " , projectDir ,  " already exists")
-    
+else:
+    print("Directory ", projectDir, " already exists")
+    exit()
 
-#change directory to  created folder
+# change directory to  created folder
 os.chdir(projectDir)
 
-#Run git init commandLine
+# Run git init commandLine
 os.system('git init')
 
-#create README.md file
+# create README.md file
 os.system('echo New Project - ' + projectName + ' >> README.md')
 
+# Using PyGithub for access Githib Api's
 
-print("Github Token "  + gitToken)
+# login into github using github token
+g = Github(gitToken)
+
+# Get user details
+user = g.get_user()
+
+# Create repo with given name
+repo = user.create_repo(projectName)
+cloneUrl = repo.clone_url
+
+# link repo
+os.system('git remote add origin ' + cloneUrl)
+os.system('git add .')
+os.system('git commit -m "Repo setup"')
+os.system('git push -u origin master')
+
+# open folder in Intellij
+os.system('idea .')
+
+print('Project setup complete.... Project will be opened in Intellij')
