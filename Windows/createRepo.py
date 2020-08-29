37a4cc1a1cba912b70dc357cf84d2fa013081877
Surya <singhsurya1012@gmail.com>
2020-08-29 20:37:34 +0530

    updated pythons script to create folder

--- Windows/createRepo.py
+++ Windows/createRepo.py
@@ -1,6 +1,24 @@
 import sys
+import os
 
 #Getting projectName from commandLine Argument passed from script
 projectName = str(sys.argv[1])
 
+#Geting project dir and github token from env variables\
+projectDir = os.environ['proj_dir']
+gitToken = os.environ['git_token']
+
+projectDir = projectDir +'/' + projectName
+
+if not os.path.exists(projectDir):
+    os.mkdir(projectDir)
+    print("Directory " , projectDir ,  " Created ")
+else:    
+    print("Directory " , projectDir ,  " already exists")
+    
+
+
+
+
+print("Github Token "  + gitToken)
 print("Creating repo: " + projectName)
