01043af7960d3b09f7103fc0e0c6baf6ad423401
Surya <singhsurya1012@gmail.com>
2020-08-29 20:07:12 +0530

    setenv script added

---
+++ Windows/setenv.bat
@@ -0,0 +1,7 @@
+@echo off
+
+setx proj_dir D:\Workspace 
+setx git_token 9332246d9ef706c5c07bb539571f673627f0b16b 
+setx PATH "%PATH%;D:\Workspace\ProjectSetupAutomation\Windows"
+
+echo Setup Complete
